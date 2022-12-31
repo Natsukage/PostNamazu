ad16c210dcf8f11a50efa6b67da0a2b1930b1b2a
夏影 <summer@summerlight.com>
2022-12-31 21:00:55 +0800

    允许通过{"c":"qid","p":"test"}方式为队列注册id，并通过{"c":"stop","p":"test"}方式打断已注册队列

--- PostNamazu/Actions/Queue.cs
+++ PostNamazu/Actions/Queue.cs
@@ -1,14 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using PostNamazu.Attributes;
 
 namespace PostNamazu.Actions
 {
     internal class Queue : NamazuModule
     {
-        [Command("queue")] [Command("DoQueueActions")]
+        private static readonly List<string> QueuePending = new(); //注册qid的队列
+
+        [Command("queue")]
+        [Command("DoQueueActions")]
         public async void DoQueue(string command)
         {
             try
@@ -19,22 +23,44 @@ namespace PostNamazu.Actions
                 if (command == "")
                     throw new Exception("指令为空");
                 var actions = JsonConvert.DeserializeObject<QueueAction[]>(command);
+                var qid = ""; //QueueID，为空时不会受到打断指令的影响
                 foreach (var action in actions)
                 {
                     await Task.Run(async () =>
                     {
                         await Task.Delay(action.d);
-                        try
+
+                        if (qid != "" && !QueuePending.Contains(qid)) //注册了qid，但是对应qid已经被移除(stop)的场合
+                            return; //打断
+                        switch (action.c.ToLower())
                         {
-                            PostNamazu.DoAction(action.c, action.p);
+                            case "qid":
+                                if (qid != "")
+                                    QueuePending.Remove(qid); //如果之前已经有旧的qid，则先移除旧的qid
+                                qid = action.p;
+                                if (qid != "")
+                                    QueuePending.Add(qid); //注册新的qid
+                                break;
+                            default:
+                                try
+                                {
+                                    PostNamazu.DoAction(action.c, action.p);
+                                }
+                                catch (Exception e)
+                                {
+                                    Log(e.ToString());
+                                }
+                                break;
                         }
-                        catch (Exception e)
-                        {
-                            Log(e.ToString());
-                        }
-                        
                     });
+                    if (qid != "" && !QueuePending.Contains(qid))
+                    {
+                        Log($"队列{qid}被要求中断");
+                        break;
+                    }
                 }
+                if (qid != "")
+                    QueuePending.Remove(qid); //执行完毕，移除队列
             }
             catch (Exception e)
             {
@@ -42,13 +68,29 @@ namespace PostNamazu.Actions
             }
 
         }
+        [Command("stop")]
+        [Command("break")]
+        [Command("BreakQueueActions")]
+        public void BreakQueue(string command)
+        {
+            Log($"要求打断{command}队列");
+            switch (command.ToLower())
+            {
+                case "all":
+                    QueuePending.Clear();
+                    break;
+                default:
+                    QueuePending.RemoveAll(qid => Regex.IsMatch(qid, $"^{command}$"));
+                    break;
+            }
+        }
 
         public class QueueAction
         {
             [JsonProperty]
-            public string? c { get; set; }
+            public string c { get; set; }
             [JsonProperty]
-            public string? p { get; set; }
+            public string p { get; set; }
             [JsonProperty]
             public int d { get; set; }
         }

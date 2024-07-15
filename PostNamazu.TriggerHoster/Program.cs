f4f12597e2a086345f682f6b4927702574f1eca1
MnFeN <self.mail.address@gmail.com>
2024-07-15 10:54:54 -0700

    适配 Triggernometry 1.2.0.2+ 的回调注册 (#65)
    添加 registrant 参数：
    https://github.com/paissaheavyindustries/Triggernometry/pull/107/commits/3f8e143ff8cadff4677d74b4b6bb678b3891384d

--- PostNamazu.TriggerHoster/Program.cs
+++ PostNamazu.TriggerHoster/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Advanced_Combat_Tracker;
 using TriggernometryProxy;
 
@@ -18,8 +19,30 @@ namespace PostNamazu.TriggerHoster
         }
         public void Init(string[] commands)
         {
+            Type triggPluginType = _triggPlugin.GetType();
+
+            // new version (> 1.2.0.1):
+            // RegisterNamedCallback(string name, CustomCallbackDelegate callback, object o, string registrant)
+            Type[] newParamTypes = new Type[] { typeof(string), typeof(ProxyPlugin.CustomCallbackDelegate), typeof(object), typeof(string) };
+
+            // old version (<= 1.2.0.1): (also available in new version)
+            // RegisterNamedCallback(string name, CustomCallbackDelegate callback, object o)  
+            Type[] oldParamTypes = new Type[] { typeof(string), typeof(ProxyPlugin.CustomCallbackDelegate), typeof(object) };
+
+            MethodInfo registerNamedCallbackMethod = triggPluginType.GetMethod("RegisterNamedCallback", newParamTypes);
+            bool isNewSignature = registerNamedCallbackMethod != null;
+            registerNamedCallbackMethod = registerNamedCallbackMethod ?? triggPluginType.GetMethod("RegisterNamedCallback", oldParamTypes);
             foreach (string command in commands)
-                TriggCallbackId.Add(_triggPlugin.RegisterNamedCallback(command, delegate (object _, string payload) { PostNamazuDelegate(command, payload); }, null));
+            {
+                List<object> parameters = new()
+                {
+                    command,
+                    new ProxyPlugin.CustomCallbackDelegate((object _, string payload) => PostNamazuDelegate(command, payload)),
+                    null
+                };
+                if (isNewSignature) parameters.Add("PostNamazu"); // registrant
+                TriggCallbackId.Add((int)registerNamedCallbackMethod.Invoke(_triggPlugin, parameters.ToArray()));
+            }
         }
         public void DeInit()
         {
